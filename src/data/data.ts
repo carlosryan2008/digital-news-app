a0dcf7af08d396388abf05e3549f5c6ae5d76df9
Andressa Colares <andressacolares133@gmail.com>
2024-08-28 15:58:14 -0300

    Normalize line endings

---
+++ src/data/data.ts
@@ -0,0 +1,94 @@
+export const navs = [
+    {
+      id: 1,
+      name: 'Home',
+      link: '/',
+      active: true,
+    },
+    {
+      id: 2,
+      name: 'Artigos',
+      link: '/postitems',
+      active: false,
+    },
+    {
+      id: 3,
+      name: 'Criar Artigo',
+      link: '/createpostitems',
+      active: false,
+    },
+    {
+      id: 4,
+      name: 'Sobre Nós',
+      link: '/about',
+      active: false,
+    },
+    {
+      id: 5,
+      name: 'Contatos',
+      link: '/contact',
+      active: false,
+    },
+  ];
+  
+  export const scis = [
+    {
+      id: 1,
+      icon: 'bi-facebook',
+      link: '',
+    },
+    {
+      id: 2,
+      icon: 'bi-twitter-x',
+      link: '',
+    },
+    {
+      id: 3,
+      icon: 'bi-instagram',
+      link: '',
+    },
+  ];
+  
+  export const heroSlides = [
+    {
+      id: 1,
+      bgImg: 'assets/img/post-slide-1.jpg',
+      title: 'Dom Pedro II: O Imperador do Brasil',
+      brief: `Dom Pedro II, nascido Pedro de Alcântara, foi o segundo imperador do Brasil, governando de 1831 a 1889. 
+      Filho de Dom Pedro I, assumiu o trono com apenas cinco anos após a abdicação de seu pai. Seu governo é marcado por avanços significativos em áreas como educação, ciência e infraestrutura, além da abolição da escravidão em 1888. Apesar de seu trabalho para modernizar o país, foi deposto em um golpe militar que instaurou a República em 1889.`,
+    },
+    {
+      id: 2,
+      bgImg: 'assets/img/post-slide-2.jpg',
+      title:
+        'Ditadura Militar Brasileira: 1964-1985',
+      brief: `A Ditadura Militar no Brasil foi um período de regime autoritário que durou de 1964 a 1985, 
+      iniciado após um golpe que depôs o presidente João Goulart. `,
+    },
+    {
+      id: 3,
+      bgImg: 'assets/img/post-slide-3.jpg',
+      title: 'Dom Pedro I: O Fundador do Brasil Independente',
+      brief: `Dom Pedro I, nascido Pedro IV de Portugal, foi o fundador e primeiro imperador do Brasil, governando de 1822 a 1831. Ele proclamou a independência do Brasil de Portugal em 1822, estabelecendo o Império do Brasil. `    },
+    {
+      id: 4,
+      bgImg: 'assets/img/post-slide-4.jpg',
+      title: ' Getúlio Vargas: O "Pai dos Pobres" e a Revolução Brasileira',
+      brief: ` Getúlio Vargas governou o Brasil de 1930 a 1945 e novamente de 1951 a 1954. Introduziu reformas sociais e trabalhistas e estabeleceu o Estado Novo, um regime autoritário. 
+      Seu governo deixou um impacto duradouro na política e na economia brasileira.`,
+    },
+  ];
+  
+  export const contact = [
+    {
+      id: 1,
+      details: {
+        name: 'Example Name',
+        email: 'info@example.com',
+        subject: 'Message',
+        message: `Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur`,
+      },
+    },
+  ];
+  
